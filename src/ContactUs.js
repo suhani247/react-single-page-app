9f8c98ac7223e434751a8e0cefbb3b28e2d99a21
Suhani Khurana <suhani24khurana@gmail.com>
2017-07-19 08:50:54 +0000

    Adding mail option

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -11,11 +11,14 @@ export default class ContactUs extends React.Component{
   	window.location.href = "mailto:suhani24khurana@gmail.com;?Subject=Query&Body=" + "Hi";
   }
 	render(){
+    var cursor = {
+      cursor : 'pointer'
+    };
 	 return(
 	 	<div className= 'inside-page'>
 		 	
           <h2>CONTACT US</h2>
-              <div className = "contactDetails"><img src="./assests/email.png" className= "icon-display" onClick={this.handleClick}/> suhani24khurana@gmail.com </div>
+              <div className = "contactDetails"><img src="./assests/email.png" className= "icon-display" onClick={this.handleClick} style={cursor}/> suhani24khurana@gmail.com </div>
               <div className = "contactDetails"><img src="./assests/phone.png" className= "icon-display"/>  9999595628 </div>
           
 	 	</div>
